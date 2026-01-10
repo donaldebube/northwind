d5fbfb2db750e9c60ad815e1292186bfa1763611
Donald Ebube <donaldebube@yahoo.com>
2026-01-10 15:01:23 +0000

    new commit – made changes to the stg_product model and finished up the dim_products model

--- models/warehouse/dim_products.sql
+++ models/warehouse/dim_products.sql
@@ -1,20 +1,23 @@
 WITH source AS (
     SELECT
-        ID AS PRODUCT_ID,
-        PRODUCT_CODE,
-        PRODUCT_NAME,
-        DESCRIPTION,
-        STANDARD_COST,
-        LIST_PRICE,
-        REORDER_LEVEL,
-        TARGET_LEVEL,
-        QUANTITY_PER_UNIT,
-        DISCONTINUED,
-        MINIMUM_REORDER_QUANTITY,
-        CATEGORY,
-        ATTACHMENTS,
-        DBT_INGESTION_TIMESTAMP AS insertion_timestamp
-    FROM {{ ref('stg_products')}}
+        products.ID AS PRODUCT_ID,
+        products.PRODUCT_CODE,
+        products.PRODUCT_NAME,
+        products.DESCRIPTION,
+        supplier.COMPANY AS SUPPLIER_COMPANY,
+        products.STANDARD_COST,
+        products.LIST_PRICE,
+        products.REORDER_LEVEL,
+        products.TARGET_LEVEL,
+        products.QUANTITY_PER_UNIT,
+        products.DISCONTINUED,
+        products.MINIMUM_REORDER_QUANTITY,
+        products.CATEGORY,
+        products.ATTACHMENTS,
+        products.DBT_INGESTION_TIMESTAMP AS insertion_timestamp
+    FROM {{ ref('stg_products')}} AS products
+    LEFT JOIN {{ ref('stg_suppliers') }} AS supplier
+        ON products.supplier_id = supplier.id
 ),
 
 unique_source AS (
@@ -27,6 +30,7 @@ SELECT
     PRODUCT_CODE,
     PRODUCT_NAME,
     DESCRIPTION,
+    SUPPLIER_COMPANY,
     STANDARD_COST,
     LIST_PRICE,
     REORDER_LEVEL,
